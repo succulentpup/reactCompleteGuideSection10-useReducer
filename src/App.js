dab7852bc204df81e246a820b7f5c8379201211d
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-05-06 09:36:19 +0100

    in Navigation component, onLogout is used thru context

--- src/App.js
+++ src/App.js
@@ -30,8 +30,11 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler}/>
+    <AuthContext.Provider value={{
+      isLoggedIn,
+      onLogout: logoutHandler,
+    }}>
+      <MainHeader/>
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler}/>}
         {isLoggedIn && <Home onLogout={logoutHandler}/>}
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -4,7 +4,7 @@ import classes from './Navigation.module.css';
 
 import AuthContext from '../../store/auth-context';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
@@ -21,7 +21,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
