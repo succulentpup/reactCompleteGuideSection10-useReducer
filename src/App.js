91bda72fe89be95af24384400b2197155048c3f5
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-05-06 09:30:11 +0100

    implemented app context feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+import AuthContext from './store/auth-context';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -28,13 +30,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler}/>
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!isLoggedIn && <Login onLogin={loginHandler}/>}
+        {isLoggedIn && <Home onLogout={logoutHandler}/>}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
