e2ac1d8caaa2fe4d9a47f6618d6d5852875df6c2
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-05-06 14:29:28 +0100

    refactor: moved context logic to auth-context from App component.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -26,9 +27,8 @@ const passwordReducer = (state, action) => {
   return { value: '', isValid: false };
 };
 
-const Login = (props) => {
-  // const [passwordState.value, setEnteredPassword] = useState('');
-  // const [passwordState.isValid, setPasswordIsValid] = useState();
+const Login = () => {
+  const authCtx = useContext(AuthContext);
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
@@ -85,7 +85,8 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    console.log('submitHandler');
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
